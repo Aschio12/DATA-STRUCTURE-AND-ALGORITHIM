0b7ae7fa3d34234c9f0d8dfbc498b3f67b50dc50
Aschalew Daraje <aschalewdere2@gmail.com>
2025-11-10 10:29:06 -0800

    Update rotate_array_k_times.py

--- leetcode/medium/rotate_array_k_times.py
+++ leetcode/medium/rotate_array_k_times.py
@@ -1,7 +1,12 @@
 nums = [1,2,3,4,5,6,7]
 k = 3
-for i in range(k):
-    tar=nums[-1]
-    nums.remove(tar)
-    nums.insert(0,tar)
-print(nums)
+if k==0:
+    print(nums)
+def rotate(l,r):
+    while l<r:
+        nums[l],nums[r]=nums[r],nums[l]
+        l+=1
+        r+=1
+rotate(0,len(nums)-1)
+rotate(0,k-1)
+rotate(k,len(nums)-1)
