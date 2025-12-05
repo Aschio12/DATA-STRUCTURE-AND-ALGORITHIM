eaa8109167cb3dbc471fceb5b79c2847cca14187
Aschalew Daraje <aschalewdere2@gmail.com>
2025-12-05 01:19:57 -0800

    Create D_X_Sum.py

---
+++ collection/D_X_Sum.py
@@ -0,0 +1,14 @@
+for _ in range(int(input)):
+    n,m=map(int,input().split())
+    matrix=[list(map(int,(input().split()))) for _ in range(n)]
+    def left(matrix):
+        for i in range(n):
+            for j in range(m):
+                left_tot=0   
+                for row in range(n):
+                    for col in range(m):
+                        l_col=
+                            
+        
+    def right(matrix):
+        
